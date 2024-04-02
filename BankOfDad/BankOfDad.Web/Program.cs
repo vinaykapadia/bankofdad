d8af906db0948cb80326be725d166cfd3b2e2d1c
Vinay Kapadia <dev@vkapadia.com>
2024-04-02 19:50:29 +0100

    Add Android APK to Web.

--- BankOfDad/BankOfDad.Web/Program.cs
+++ BankOfDad/BankOfDad.Web/Program.cs
@@ -25,7 +25,7 @@ builder.Services
     .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
-        options.TokenValidationParameters = new TokenValidationParameters()
+        options.TokenValidationParameters = new TokenValidationParameters
         {
             ClockSkew = TimeSpan.Zero,
             ValidateIssuer = true,
